52bcbc5d9c73b48075cae0a12416eb3e036526bb
hbery <a.twardosz98@gmail.com>
2022-07-17 20:20:05 +0200

    feat: add installation scripts for applying certain configurations, in new repo

---
+++ _installation-scripts/install-kde-themes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# install kde McMojave theme
+
+# install McMojave theme for KDE
+git clone https://github.com/vinceliuice/McMojave-kde.git /tmp/McMojave
+chmod u+x /tmp/McMojave/install.sh
+/tmp/McMojave/install.sh
---
+++ _installation-scripts/install-xfce-themes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# simple bootstrapping script to quickly install gtk themes for XFCE
+
+git clone https://github.com/vinceliuice/Mojave-gtk-theme.git /tmp/McMojave-gtk
+chmod u+x /tmp/McMojave-gtk/install.sh
+/tmp/McMojave-gtk/install.sh
