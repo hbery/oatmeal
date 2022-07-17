beb559850777f11b9398a572cb9f4a5f4f08f278
hbery <a.twardosz98@gmail.com>
2022-07-17 22:00:00 +0200

    feat: modify install-icons.sh to prevent accidental source-and-run of a script

--- _icons/install-icons.sh
+++ _icons/install-icons.sh
@@ -1,21 +1,66 @@
 #!/bin/sh
-#
-# simple bootstrapping script to get and engage install-scripts for icons
+# vim: ft=bash : ts=2 : sw=2 :
+# ~~~
+# Date: 2022
+# Author: Adam Twardosz (github.com/hbery)
+# Purpose: simple bootstrapping script to get
+#   and engage install-scripts for icons
+# ~~~
 
 _iconInstallDirectory="${HOME}/.local/share/icons"
-[ ! -d "${_iconInstallDirectory}" ] && mkdir "${_iconInstallDirectory}"
 
-# get Tela-circle-icon-theme and install locally
-git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git /tmp/Tela-circle
-chmod u+x /tmp/Tela-circle/install.sh
-/tmp/Tela-circle/install.sh -a -d "${_iconInstallDirectory}"
+### BEGIN: FUNCTION_SECTION {
+## BEGIN _installTelaCircleIconsFn {
+_installTelaCircleIconsFn () {
+  # get Tela-circle-icon-theme and install locally
+  git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git /tmp/Tela-circle
+  chmod u+x /tmp/Tela-circle/install.sh
+  /tmp/Tela-circle/install.sh -a -d "${_iconInstallDirectory}"
+}
+## . END _installTelaCircleIconsFn }
 
-# get Tela-icon-theme and install locally
-git clone https://github.com/vinceliuice/Tela-icon-theme.git /tmp/Tela
-chmod u+x /tmp/Tela/install.sh
-/tmp/Tela/install.sh -a -d "${_iconInstallDirectory}"
+## BEGIN _installTelaIconsFn {
+_installTelaIconsFn () {
+  # get Tela-icon-theme and install locally
+  git clone https://github.com/vinceliuice/Tela-icon-theme.git /tmp/Tela
+  chmod u+x /tmp/Tela/install.sh
+  /tmp/Tela/install.sh -a -d "${_iconInstallDirectory}"
+}
+## . END _installTelaIconsFn }
 
-# get papirus-icon-theme and install locally
-git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git /tmp/papirus
-chmod u+x /tmp/papirus/install.sh
-DESTDIR="${_iconInstallDirectory}" /tmp/papirus/install.sh
+## BEGIN _installPapirusIconsFn {
+_installPapirusIconsFn () {
+  # get papirus-icon-theme and install locally
+  wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="${_iconInstallDirectory}" sh
+}
+## . END _installPapirusIconsFn }
+
+## BEGIN _installQogirIconsFn {
+_installQogirIconsFn () {
+  # get Qogir-icon-theme and install locally
+  git clone https://github.com/vinceliuice/Qogir-icon-theme.git /tmp/Qogir
+  chmod u+x /tmp/Qogir/install.sh
+  /tmp/Qogir/install.sh -d "${_iconInstallDirectory}"
+}
+## . END _installQogirIconsFn }
+
+## BEGIN _mainFn {
+_mainFn () {
+  [ ! -d "${_iconInstallDirectory}" ] \
+    && mkdir "${_iconInstallDirectory}"
+
+  _installTelaCircleIconsFn
+  _installTelaIconsFn
+  _installPapirusIconsFn
+  _installQogirIconsFn
+}
+## . END _mainFn }
+### . END: FUNCTION_SECTION }
+
+
+### BEGIN: MAIN_SECTION {
+if [ "${BASH_SOURCE[0]}" = "$0" ]
+then
+  _mainFn "$@"
+fi
+### . END: MAIN_SECTION }
