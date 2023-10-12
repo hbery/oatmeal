d3e7f0a7b30fc4925d5033a57f8134e87a9a04fa
hbery <a.twardosz98@gmail.com>
2023-10-12 09:27:45 +0200

    fix(neovim): fix after indent-blankline.nvim config change

--- neovim/.config/nvim/lua/lazy-plugins.lua
+++ neovim/.config/nvim/lua/lazy-plugins.lua
@@ -119,9 +119,11 @@ require('lazy').setup({
 
   {
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = '┊',
+      },
     },
   },
 
