b380b9b9ab4948fab75d20b05d756b29baff9237
hbery <a.twardosz98@gmail.com>
2024-06-22 23:56:26 +0200

    docs: add license to the script

--- _system/install-hyprland.sh
+++ _system/install-hyprland.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 # vim: ft=bash : ts=4 : sts=4 : sw=4 : et :
 #
-# install hyprland + deps on debian 12
+# ~~~
+#
+# $date: 2024
+# $author: Adam Twardosz (github.com/hbery)
+# $description:
+#   Install Hyprland Wayland Compositor alongside
+#   with the dependencies and addons.
+#
+# ~~~
+# MIT License
+#
+# Copyright (c) 2024 Adam Twardosz (github.com/hbery)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ~~~
 
 set -euo pipefail
 source /etc/os-release
