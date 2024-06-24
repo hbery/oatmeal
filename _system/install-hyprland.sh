6db2872c18c16058e3d6e594d4edbe00ba5a2bd6
hbery <a.twardosz98@gmail.com>
2024-06-24 21:43:48 +0200

    chore: add missing dependency

--- _system/install-hyprland.sh
+++ _system/install-hyprland.sh
@@ -148,7 +148,7 @@ _commonPackages=(
     libgtk-3-dev libsystemd-dev edid-decode extra-cmake-modules libpam0g-dev qtbase5-dev qtdeclarative5-dev
     qttools5-dev python3-docutils check qt6-base-dev qt6-tools-dev qt6-declarative-dev libtomlplusplus-dev
     librsvg2-dev libzip-dev libpugixml-dev libxcb-util-dev libwlroots-dev automake xutils-dev libtool xcb-proto
-    xwayland
+    xwayland libtomlplusplus-dev
 )
 
 _errMsg () { >&2 echo -e "${_redClr}ERROR:${_norClr}${_bldClr} $*${_norClr}"; }
